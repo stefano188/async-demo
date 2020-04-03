d4e96b1c629206e604f3c263f92a6417d4694ce2
Stefano Mazzolini <stefano.mazzolini@gmail.com>
2020-04-03 17:46:32 +0200

    async demo named functions

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 
 console.log('Before');
-getUser(1, (user) => {
-    console.log('user', user);
-    getRepositorie(user.gitHubUsername, (repos) => {
-        console.log('repositories..', repos);
-        // CALLBACK HELL or Christams Tree
-    });
-});
+getUser(1, findUser);
 console.log('After');
 
 function getUser(id, callback) {
     setTimeout(() => {
         console.log('Reading user from database...');
         callback({id: id, gitHubUsername: 'mosh'});
-    }, 2000);
+    }, 1200);
 }
 
 function getRepositorie(username, callback) {
     setTimeout(() => {
         console.log('Reading repositories from user', username);
         callback(['repo1','repo2']);
-    }, 2000);
+    }, 1200);
+}
+
+function getCommits(repo, callback) {
+    setTimeout(() => {
+        callback(['one','two']);
+    }, 1000);
+}
+
+function displayCommits(commits) {
+    console.log('logging commits', commits);
+}
+
+function findRepositories(repos) {
+    console.log('repositories..', repos);
+    const repo = repos[0];
+    getCommits(repo, displayCommits);
+}
+
+function findUser(user) {
+    console.log('user', user);
+    getRepositorie(user.gitHubUsername, findRepositories);
 }
